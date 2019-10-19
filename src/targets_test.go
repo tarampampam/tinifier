4220378f5beddd8ee9e6d9aa015ede5aa5a39e57
Paramtamtam <slashkot@gmail.com>
2019-10-19 14:00:13 +0500

    WIP

--- src/targets_test.go
+++ src/targets_test.go
@@ -9,6 +9,71 @@ import (
 	"time"
 )
 
+func TestTargetsLoad(t *testing.T) {
+	dir := createTempDir()
+
+	createFilesAndDirs(
+		[][]string{{dir, "baz"}, {dir, "zzz"}},
+		[][]string{{dir, "bar.a"}, {dir, "bar.b"}, {dir, "baz", "foo.a"}, {dir, "baz", "foo.b"}},
+	)
+
+	defer func() {
+		if err := os.RemoveAll(dir); err != nil {
+			panic(err)
+		}
+	}()
+
+	targets = Targets{}
+
+	type testCase struct {
+		targets    []string
+		extensions []string
+		expected   []string
+	}
+
+	var asserts = []testCase{
+		{
+			targets:    []string{dir},
+			extensions: []string{".a"},
+			expected:   []string{filepath.Join(dir, "bar.a")},
+		},
+		{
+			targets:    []string{dir},
+			extensions: []string{".a", ".b"},
+			expected:   []string{filepath.Join(dir, "bar.a"), filepath.Join(dir, "bar.b")},
+		},
+		{
+			targets:    []string{dir},
+			extensions: []string{".a,.b"},
+			expected:   []string{filepath.Join(dir, "bar.a"), filepath.Join(dir, "bar.b")},
+		},
+		{
+			targets:    []string{dir, filepath.Join(dir, "baz"), filepath.Join(dir, "zzz")},
+			extensions: []string{".a"},
+			expected:   []string{filepath.Join(dir, "bar.a"), filepath.Join(dir, "baz", "foo.a")},
+		},
+	}
+
+	for _, testCase := range asserts {
+		targets = Targets{}
+		targets.Load(testCase.targets, &testCase.extensions)
+
+		// Test size
+		if len(targets.Files) != len(testCase.expected) {
+			t.Error(
+				"For", testCase.targets,
+				"expected result count", len(testCase.expected),
+				"but got", len(targets.Files),
+			)
+		}
+
+		// Test expected entries
+		for _, expectedEntry := range testCase.expected {
+			AssertStringSliceContainsString(targets.Files, expectedEntry, t)
+		}
+	}
+}
+
 func TestFilterFilesUsingExtensions(t *testing.T) {
 	type testCase struct {
 		values     []string
@@ -44,40 +109,18 @@ func TestFilterFilesUsingExtensions(t *testing.T) {
 }
 
 func TestTargetsToFiles(t *testing.T) {
-	var mkFile = func(pathElem ...string) *os.File {
-		if f, err := os.Create(filepath.Join(pathElem...)); err != nil {
-			panic(err)
-		} else {
-			return f
-		}
-	}
-	var mkDir = func(pathElem ...string) string {
-		path := filepath.Join(pathElem...)
-		if err := os.Mkdir(path, 0777); err != nil {
-			panic(err)
-		}
-		return path
-	}
+	dir := createTempDir()
 
-	// Create directory in temporary
-	dir, err := ioutil.TempDir("", "test-")
-	if err != nil {
-		t.Error(err)
-	}
+	createFilesAndDirs(
+		[][]string{{dir, "baz"}, {dir, "zzz"}},
+		[][]string{{dir, "bar.a"}, {dir, "bar.b"}, {dir, "baz", "foo.a"}, {dir, "baz", "foo.b"}},
+	)
 
 	defer func() {
 		if err := os.RemoveAll(dir); err != nil {
-			t.Error(err)
+			panic(err)
 		}
 	}()
-
-	// Create files structure
-	mkFile(dir, "bar.a")
-	mkFile(dir, "bar.b")
-	mkDir(dir, "baz")
-	mkFile(dir, "baz", "foo.a")
-	mkFile(dir, "baz", "foo.b")
-	mkDir(dir, "zzz")
 
 	type testCase struct {
 		targets   []string
@@ -87,7 +130,7 @@ func TestTargetsToFiles(t *testing.T) {
 
 	var asserts = []testCase{
 		{
-			targets:  []string{filepath.Join(dir)},
+			targets:  []string{dir},
 			expected: []string{filepath.Join(dir, "bar.a"), filepath.Join(dir, "bar.b")},
 		},
 		{
@@ -95,7 +138,7 @@ func TestTargetsToFiles(t *testing.T) {
 			expected: []string{filepath.Join(dir, "baz", "foo.a"), filepath.Join(dir, "baz", "foo.b")},
 		},
 		{
-			targets: []string{filepath.Join(dir), filepath.Join(dir, "baz")},
+			targets: []string{dir, filepath.Join(dir, "baz")},
 			expected: []string{
 				filepath.Join(dir, "bar.a"), filepath.Join(dir, "bar.b"),
 				filepath.Join(dir, "baz", "foo.a"), filepath.Join(dir, "baz", "foo.b"),
@@ -134,8 +177,6 @@ func TestTargetsToFiles(t *testing.T) {
 	}
 }
 
-// @todo: write more tests
-
 // Assert that strings slice contains expected string
 func AssertStringSliceContainsString(slice []string, expected string, t *testing.T) {
 	for _, item := range slice {
@@ -145,4 +186,28 @@ func AssertStringSliceContainsString(slice []string, expected string, t *testing
 	}
 
 	t.Error("In", slice, "value", expected, "was not found")
+}
+
+// Prepare files structure (create files and directories).
+func createFilesAndDirs(dirs [][]string, files [][]string) {
+	for _, d := range dirs {
+		if err := os.Mkdir(filepath.Join(d...), 0777); err != nil {
+			panic(err)
+		}
+	}
+
+	for _, f := range files {
+		if _, err := os.Create(filepath.Join(f...)); err != nil {
+			panic(err)
+		}
+	}
+}
+
+// Create directory in temporary
+func createTempDir() string {
+	if dir, err := ioutil.TempDir("", "test-"); err != nil {
+		panic(err)
+	} else {
+		return dir
+	}
 }
