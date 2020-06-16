3921925859bc92df5f9d9238c77dc9771cc5418a
Paramtamtam <slashkot@gmail.com>
2020-06-16 19:29:56 +0500

    WIP

--- cmd/compress/command.go
+++ cmd/compress/command.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/signal"
 	"sync"
-	"sync/atomic"
 	"syscall"
 	"time"
 	"tinifier/cmd/shared"
@@ -32,28 +31,30 @@ func (cmd *Command) Handle(l *log.Logger, _ []string) error {
 	var (
 		totalSavedKb int64
 		wg           = sync.WaitGroup{}
-		tasks        = make(chan task)
-		oss          = make(chan os.Signal, 1) // channel for operational system signals
+		tasksChan    = make(chan task, cmd.Threads)
+		ossChan      = make(chan os.Signal, 1) // channel for operational system signals
 	)
 
 	// "subscribe" for system signals
-	signal.Notify(oss, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	signal.Notify(ossChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 
 	// create context
 	ctx, cancel := context.WithCancel(context.Background())
 
+	tasks, _ := cmd.getTasks(l)
+
 	// fill-up tasks channel
-	go cmd.pushTasks(l, ctx, tasks)
+	go cmd.pushTasks(l, tasks, tasksChan)
 
 	// start workers
 	for i := 0; i < cmd.Threads; i++ {
 		wg.Add(1)
-		go cmd.work(l, ctx, tasks, &totalSavedKb, &wg)
+		go cmd.work(l, ctx, tasksChan, &wg)
 	}
 
 	// listen for system signals in separate goroutine
 	go func() {
-		s := <-oss
+		s := <-ossChan
 		l.WithField("signal", s).Warn("Caught stopping signal")
 		cancel()
 	}()
@@ -65,29 +66,26 @@ func (cmd *Command) Handle(l *log.Logger, _ []string) error {
 	return nil
 }
 
-func (cmd *Command) pushTasks(l *log.Logger, ctx context.Context, tasks chan task) {
-	defer close(tasks)
-
-	var input []task
+func (cmd *Command) getTasks(_ *log.Logger) (*[]task, error) {
+	var tasks []task
 
 	for i := 0; i < 10; i++ { // @todo: just for a test - generate slice with some tasks
-		input = append(input, task{num: uint32(i)})
+		tasks = append(tasks, task{num: uint32(i)})
 	}
 
-	for _, task := range input {
-		select {
-		case <-ctx.Done(): // if `cancel()` executed
-			l.Debug("stopping tasks pushing")
-			return
+	return &tasks, nil
+}
 
-		default:
-			l.WithField("num", task.num).Debug("push new task", task)
-			tasks <- task
-		}
+func (cmd *Command) pushTasks(l *log.Logger, from *[]task, to chan task) {
+	defer close(to)
+
+	for _, task := range *from {
+		l.WithField("num", task.num).Debug("push new task", task)
+		to <- task
 	}
 }
 
-func (cmd *Command) work(l *log.Logger, ctx context.Context, tasks chan task, totalSavedKb *int64, wg *sync.WaitGroup) {
+func (cmd *Command) work(l *log.Logger, ctx context.Context, tasks chan task, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
@@ -106,7 +104,6 @@ func (cmd *Command) work(l *log.Logger, ctx context.Context, tasks chan task, to
 
 			time.Sleep(1 * time.Second)
 			l.WithField("num", task.num).Debug("worker process task", task)
-			atomic.AddInt64(totalSavedKb, 1)
 		}
 	}
 }
