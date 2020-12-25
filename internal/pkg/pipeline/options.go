36940eefa8477d01b7cda2f27379086142b68189
Paramtamtam <slashkot@gmail.com>
2020-12-25 12:17:30 +0500

    WIP

--- internal/pkg/pipeline/options.go
+++
@@ -1,9 +0,0 @@
-package pipeline
-
-// CompressingPipelineOption allows to setup some internal pipeline properties from outside.
-type CompressingPipelineOption func(*CompressingPipeline)
-
-// WithPreWorkerRun setups pre worker run handler.
-func WithPreWorkerRun(h TaskHandler) CompressingPipelineOption {
-	return func(p *CompressingPipeline) { p.preWorkerRun = h }
-}
